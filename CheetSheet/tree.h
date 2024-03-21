bbb2daea13faff23ec4709ad0e1acd5e06c70ebc
Anshu Ahirwar <anshuahi@Anshus-MacBook-Air.local>
2024-03-21 18:03:42 +0530

    POTD 21-march-2024

---
+++ CheetSheet/tree.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
---
+++ ProblemOfTheDay/21_03_2024.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <queue>
+#include "tree.h"
+using namespace std;
+
+class Solution{
+    public:
+    //Function to store the zig zag order traversal of tree in a list.
+    vector<int> solveUsingQueue(Node* root) {
+        if(!root)
+    	    return {};
+    	vector<int> ans, temp;
+    	queue<Node*> q;
+    	q.push(root);
+    	bool level = true;  // setting the even level as true, i.e. false means odd level
+    	while(!q.empty()){
+    	    int sz = q.size();
+    	    temp.clear();
+    	    //  traverse one layer at a time and store all the values in temp array;
+    	    while(sz--){
+    	        Node* node = q.front();
+    	        q.pop();
+    	        temp.push_back(node->data);
+    	        if(node->left){
+    	            q.push(node->left);
+    	        }
+    	        if(node->right){
+    	            q.push(node->right);
+    	        }
+    	    }
+    	    // if odd level, reverse the temp array;
+    	    if(!level){ 
+    	       reverse(temp.begin(), temp.end());
+    	    }
+    	    for(auto x: temp){
+    	        ans.push_back(x);
+    	    }
+    	    level = !level;
+    	}
+    	
+    	return ans;
+    }
+    
+    vector<int> solveUsingStack(Node* root) {
+        if(!root)
+    	    return {};
+    	    
+    	vector<int> ans;
+    	stack<Node*> st1, st2;
+    	st1.push(root);
+    	while(!st1.empty() || !st2.empty()){
+    	    //  traverse one layer at a time and store all the values in ans array;
+    	    while(!st1.empty()){
+    	        Node* node = st1.top();st1.pop();
+    	        ans.push_back(node->data);
+    	        if(node->left){
+    	            st2.push(node->left);
+    	        }
+    	        if(node->right){
+    	            st2.push(node->right);
+    	        }
+    	    }
+    	    while(!st2.empty()) {
+    	        Node* node = st2.top(); st2.pop();
+    	        ans.push_back(node->data);
+    	        if(node->right){
+    	            st1.push(node->right);
+    	        }
+    	        if(node->left)
+    	            st1.push(node->left);
+    	    }
+    	}
+    	
+    	return ans;
+    }
+    
+    //Function to store the zig zag order traversal of tree in a list.
+    vector <int> zigZagTraversal(Node* root)
+    {
+    	return solveUsingQueue(root); // solution 1
+        // return solveUsingStack(root); // solution 2
+    }};
+
+int main(){
+    return 0;
+}
