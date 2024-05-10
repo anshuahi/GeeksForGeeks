e7eb33a583708d8f6e1ae4ea1d8e310a1f2b4b5f
Anshu Ahirwar <anshuahi@Anshus-MacBook-Air.local>
2024-05-10 07:43:27 +0530

    10-may 2024: POTD solution

--- ProblemOfTheDay/10_05_2024.cpp
+++ ProblemOfTheDay/10_05_2024.cpp
@@ -9,3 +9,35 @@ Approach
     3. use backtracking with base conditions(rem == 0 and rem < 0 || idx >= n)
 
 */
+class Solution
+{
+public:
+    set<vector<int>> st;
+    void solve(int idx, vector<int> &arr, vector<int> &v, int rem)
+    {
+        int n = arr.size();
+        if (rem == 0)
+        {
+            st.insert(v);
+            return;
+        }
+        if (rem < 0 || idx >= n)
+            return;
+        v.push_back(arr[idx]);
+        solve(idx + 1, arr, v, rem - arr[idx]);
+        v.pop_back();
+        solve(idx + 1, arr, v, rem);
+    }
+    vector<vector<int>> CombinationSum2(vector<int> arr, int n, int k)
+    {
+        vector<int> v;
+        sort(arr.begin(), arr.end());
+        solve(0, arr, v, k);
+        vector<vector<int>> ans;
+        for (auto x : st)
+        {
+            ans.push_back(x);
+        }
+        return ans;
+    }
+};
