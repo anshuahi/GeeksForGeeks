b1d13e10ab98c78a67325d8425d4a940dcecb7dc
Anshu Ahirwar <anshuahi@Anshus-MacBook-Air.local>
2024-05-10 07:43:07 +0530

    10-may 2024: POTD solution

---
+++ ProblemOfTheDay/10_05_2024.cpp
@@ -0,0 +1,11 @@
+/*
+GeeksForGeeks-POTD: Combination Sum II
+
+POTD Link: https://www.geeksforgeeks.org/problems/combination-sum-ii-1664263832/1
+
+Approach
+    1. sort the array
+    2. try all combinations
+    3. use backtracking with base conditions(rem == 0 and rem < 0 || idx >= n)
+
+*/
