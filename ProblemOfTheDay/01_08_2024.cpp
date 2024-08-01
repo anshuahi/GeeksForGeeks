1898d4a914ff55f8cad16d0372b0ad741d56e77d
Anshu Ahirwar <anshuahirwar.dakshana2015@gmail.com>
2024-08-02 03:28:04 +0530

    01-Aug 2024: Spirally traversing a matrix

---
+++ ProblemOfTheDay/01_08_2024.cpp
@@ -0,0 +1,55 @@
+/*
+GeeksForGeeks-POTD: Spirally traversing a matrix
+
+POTD Link: https://www.geeksforgeeks.org/problems/spirally-traversing-a-matrix-1587115621/1
+
+Approach
+1. count the number of subsets whose sum is equal to (target-d)/2
+
+*/
+
+class Solution
+{
+public:
+    vector<int> spirallyTraverse(vector<vector<int>> &matrix)
+    {
+        vector<int> ans;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int left = 0, right = m - 1;
+        int top = 0, bottom = n - 1;
+
+        while (top <= bottom && left <= right)
+        {
+            // go -> left to right
+            for (int j = left; j <= right; j++)
+                ans.push_back(matrix[top][j]);
+            // update top
+            top++;
+
+            // go -> top to bottom
+            for (int i = top; i <= bottom; i++)
+                ans.push_back(matrix[i][right]);
+            // update right
+            right--;
+
+            // special check if n is odd for top and bottom merge
+            if (top > bottom || left > right)
+                break;
+
+            // go -> right to left
+            for (int j = right; j >= left; j--)
+                ans.push_back(matrix[bottom][j]);
+            // update bottom
+            bottom--;
+
+            // go -> bottom to top
+            for (int i = bottom; i >= top; i--)
+                ans.push_back(matrix[i][left]);
+            // update left
+            left++;
+        }
+
+        return ans;
+    }
+};
